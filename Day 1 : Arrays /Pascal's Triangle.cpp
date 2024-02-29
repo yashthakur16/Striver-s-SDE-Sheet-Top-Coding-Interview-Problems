9e7ee9de66413095050e90ac904c29b2cf463d27
Yash Thakur <117333139+yashthakur16@users.noreply.github.com>
2024-02-29 22:42:41 +0530

    Create Pascal's Triangle.cpp

---
+++ Day 1 : Arrays /Pascal's Triangle.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) 
+    {
+        int N=numRows;
+      vector<vector<int>> ans;
+    vector<int> prev;
+
+    for(int i=0;i<2&&i<N;i++)
+    {
+        vector<int>row(i+1);
+        row[0]=1;
+        row[i]=1;
+        ans.push_back(row);
+        prev=row;
+    }
+
+
+
+    for(int i=2;i<N;i++)
+    {
+        vector<int>row;
+        row.push_back(1);
+        for(int j=0;j<prev.size()-1;j++)
+        {
+            row.push_back(prev[j]+prev[j+1]);
+        }
+        row.push_back(1);
+        prev.clear();
+
+        prev=row;
+
+
+        ans.push_back(row);
+    }
+
+
+    return ans;
+    }
+};
